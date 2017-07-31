80d5c5a4ba28950936b512554b4ed3e88440f447
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2017-07-30 22:45:12 -0700

    wtd: registration

--- tutorapp/urls.py
+++ tutorapp/urls.py
@@ -1,13 +1,29 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+# from django.conf.urls.static import static
 # Add this import
 from django.contrib.auth import views
-from log.forms import LoginForm
+from log.forms import UserLoginForm
+from log.views import (login_view, register_view, logout_view)
+from . import views
+# urlpatterns = [
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'', include('log.urls')),
+#     # url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
+#     url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': UserLoginForm}, name='login'),
+#     url(r'^logout/$', views.logout, {'next_page': '/login'},name='logout'),  
+# ]
+
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('log.urls')),
-    # url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
-    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^logout/$', views.logout, {'next_page': '/login'},name='logout'),  
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^register/', register_view, name='register'),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tutorapp/views.py
+++ tutorapp/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def stub(request):
-    return render(request, 'tutorapp/stub.html', {})
-
-def login(request):
-	return render(request, 'tutorapp/login.html', {})
-
-# def logout(request):
-# 	return render(request, 'tutorapp/stub.html', {})
+def home(request):
+    return render(request, 'home.html', {})
