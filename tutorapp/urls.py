3b6c69df2b7066824ae8d974f0c61d0c6c4ffee8
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2017-07-30 21:03:19 -0700

    wtd: connect login form with user

--- tutorapp/urls.py
+++ tutorapp/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import url
-from . import views
-
+from django.conf.urls import include, url
+from django.contrib import admin
+# Add this import
+from django.contrib.auth import views
+from log.forms import LoginForm
 
 urlpatterns = [
-    url(r'^$', views.stub, name='stub'),
-    url(r'^login/$', views.login, name='login'),
-    # url(r'^logout/$', views.logout, name='logout'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('log.urls')),
+    # url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'},name='logout'),  
 ]
