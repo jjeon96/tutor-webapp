70a2aa09074752658b7d81f89156e85609fc65fc
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2017-07-30 22:20:49 -0700

    2ndversion use as reference for registration

--- tutorapp/urls.py
+++ tutorapp/urls.py
@@ -1,13 +1,18 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
-# Add this import
-from django.contrib.auth import views
-from log.forms import LoginForm
+
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('log.urls')),
-    # url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
-    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^logout/$', views.logout, {'next_page': '/login'},name='logout'),  
+    
+    url(r'^admin/', admin.site.urls),
+    # url(r'^comments/', include("comments.urls", namespace='comments')),
+    
+    url(r'^register/', register_view, name='register'),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
+    # url(r'^', include("posts.urls", namespace='posts')),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
