0dc71ea61a7ea7a6b71e6d8d8ad128b42b109a79
jamie <jamiejeon96@gmail.com>
2017-08-02 16:25:44 -0700

    my work

--- tutorapp/urls.py
+++ tutorapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.stub, name='stub'),
+    url(r'^post_list$', views.post_list, name='post_list'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$',views.login,name='login'),
+    url(r'^omfg$', views.omfg, name ='omfg'),
 ]
--- tutorapp/views.py
+++ tutorapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.utils import timezone
+#from .models import #.....  (user)
+#from .models import #..... (Post)
 
 # Create your views here.
 def stub(request):
     return render(request, 'tutorapp/stub.html', {})
+
+def post_list(request):
+	posts = Post.objects.filter(posted_date_lte=timezone.now()).order_by('posted_date')
+	return render(request, 'tutorapp/post_list.html', {'posts':posts})
+
+def register(request):
+	return render(request, 'tutorapp/register.html',{})
+
+def login(request):
+	return render(request, 'tutorapp/login.html', {})
+
+def omfg(request):
+	return render(request, 'tutorapp/omfg.html',{})
