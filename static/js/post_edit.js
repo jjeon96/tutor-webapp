d3afb43aa5b2d2f29e2ee5897c6ec9b7f34faf6d
jamie <jamiejeon96@gmail.com>
2017-08-14 17:26:56 -0700

    css changed

--- static/js/post_edit.js
+++ static/js/post_edit.js
@@ -7,7 +7,54 @@ $(document).ready(function(){
 	    startTime: '8:00am',
 	    dynamic: true,
 	    dropdown: true,
-	    scrollbar: true
+	    scrollbar: true,
 	});
 });
 
+
+	    // change: function(time) {
+     //        // the input field
+     //        var element = $(this), text;
+     //        // get access to this Timepicker instance
+     //        var timepicker = element.timepicker();
+     //        text = 'Selected time is: ' + timepicker.format(time);
+     //        element.siblings('span.help-line').text(text);
+     //    }
+ 
+// 	});
+
+// $(function() {
+//   $('#mondaystart').timepicker();
+//   $('#mondayend').timepicker();
+
+//   function timeToInt(time) {
+//     var arr = time.match(/^(0?[1-9]|1[012]):([0-5][0-9])([APap][mM])$/);
+//     if (arr == null) return -1;
+
+//     if (arr[3].toUpperCase() == 'PM') {
+//       arr[1] = parseInt(arr[1]) + 12;
+//     }
+//     return parseInt(arr[1]*100) + parseInt(arr[2]);
+//   }
+
+//   function checkDates() {
+//     if (($('#mondaystart').val() == '') || ($('#mondayend').val() == '')) return;
+
+//     var start = timeToInt($('#mondaystart').val());
+//     var end = timeToInt($('#mondayend').val());
+
+//     if ((start == -1) || (end == -1)) {
+//       alert("Start or end time it's not valid");
+//     }
+
+//     if (start > end) {
+//       alert('Start time should be lower than end time');
+//     }
+
+//   }
+
+//   $('#mondaystart').on('change', checkDates);
+//   $('#mondayend').on('change', checkDates);
+// });
+
+// });
