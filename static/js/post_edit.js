b67ae3831d9968ce612a3120c2797f90556a71d1
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2017-09-12 23:32:57 -0700

    validateTime bug fixed, created multiple helper functions, creates a time object. Need to synchronize it to backend objects layout. WTD: ajax call, pass post pk_id for security reason.

--- static/js/post_edit.js
+++ static/js/post_edit.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
-    var tutorAvailTime = [];
+    var tutorAvailableTime = [];
     var numOfTimeObjects = 0;
     // $('.postedit_time').hide();
 
-
-
+  function convertTimeFormat(str) {
+        var time = str;
+        var hours = Number(time.match(/^(\d+)/)[1]);
+        var minutes = Number(time.match(/:(\d+)/)[1]);
+        var AMPM = time.match(/\s(.*)$/)[1];
+        if (AMPM == "PM" && hours < 12) hours = hours + 12;
+        if (AMPM == "AM" && hours == 12) hours = hours - 12;
+        var sHours = hours.toString();
+        var sMinutes = minutes.toString();
+        if (hours < 10) sHours = "0" + sHours;
+        if (minutes < 10) sMinutes = "0" + sMinutes;
+        return (sHours + ":" + sMinutes);
+  }
   function timeToInt(time) {
-    var arr = time.match(/^(0?[1-9]|1[012]):([0-5][0-9])([APap][mM])$/);
-    if (arr == null) return -1;
+    var formatedTime = convertTimeFormat(time);
+    var hoursMinutes = formatedTime.split(/[.:]/);
+    var hours = parseInt(hoursMinutes[0], 10);
+    var minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+    var convertedTime = (hours + minutes / 60);
 
-    if (arr[3].toUpperCase() == 'PM') {
-      arr[1] = parseInt(arr[1]) + 12;
-    }
-    return parseInt(arr[1]*100) + parseInt(arr[2]);
+    if (convertedTime == null) return -1;
+
+    return convertedTime;
   }
 
   function checkDates() {
@@ -31,14 +44,20 @@ $(document).ready(function(){
 
   }
 
+  //need a valid work flow for alerting
   function validateTime(startTime, endTime) {
+      console.log("start Time is " + startTime);
+      console.log("end Time is " + endTime);
       if ((startTime == -1) || (endTime == -1)) {
       alert("Start or end time it's not valid");
+      return false;
     }
 
     if (startTime > endTime) {
       alert('Start time should be lower than end time');
+      return false;
     }
+      return true;
   }
 
   $(".add_tutor_time").on('click',function() {
@@ -66,16 +85,29 @@ $(document).ready(function(){
           scrollbar: true,
       });
 
+      //made it for now
       $(".tutor_time_edit").hide();
       $(".tutor_time_delete").hide();
+      //code for creating a time object,
+      //currently contains a bug, may need a different workflow
       $(".tutor_time_save").on("click", function() {
           var parentDiv = $(this).parent();
           var parentDivClass = parentDiv.attr("class");
           var startTime = $("#" +startTimeId).val();
           var endTime = $("#"+endTimeId).val();
           var dayOfWeek = $("."+daysofweekClass).val();
-          console.log("hello " +  parentDivClass + " start time is " + startTime + " end time is " + endTime);
-          validateTime(timeToInt(startTime), timeToInt(endTime));
+
+          if (validateTime(timeToInt(startTime), timeToInt(endTime))) {
+              var timeObject = {"startTime": startTime,
+                                "endTime": endTime,
+                                "dayOfWeek": dayOfWeek}
+              tutorAvailableTime.push(timeObject);
+            // console.log("hello " +  parentDivClass + " start time is " + startTime + " end time is " + endTime);
+              console.log(tutorAvailableTime);
+          } else {
+              console.log("Time Validation has failed");
+          }
+
       });
   });
 
